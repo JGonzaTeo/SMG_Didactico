82d771c1415d0699a5553fd2c2c8b50315df7df4
Fredy-de-leon <fedel21m@gmail.com>
2020-02-10 08:05:57 -0600

    prueba 1

---
+++ SMG/CapaDatos/Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace CapaDatos
+{
+   public class Conexion
+    {
+        public OdbcConnection conexionbd()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=cine");// creacion de la conexion via ODBC
+
+            try
+            {
+
+                conn.Open();
+            }
+            catch (OdbcException ex)
+            {
+                Console.WriteLine("No se pudo realizar la conexión");
+            }
+            return conn;
+        }
+
+    }
+}
